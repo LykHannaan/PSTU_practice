b6feb107ff73f186a7745484b6a8b33834125b65
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2020-11-13 02:14:53 +0500

    Update 2_4.cpp

--- 2_4/2_4.cpp
+++ 2_4/2_4.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
 
+int n; //длина основания.
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите длину основания:" << endl;
+	cin >> n;
+	int p = n / 2; //количество " ".
+	int z = 1;  //количество "*".
+
+	if ((n <= 3) or (n % 2 == 0))
+		cout << "Не соответствует условию задачи!";
+	else
+	{
+		while (z <= n)
+		{
+			for (int j = 1; j <= p; j++)
+			{
+				cout << " ";
+			}
+			for (int j = 1; j <= z; j++)
+			{
+				cout << "*";
+			}
+			p--;
+			z += 2;
+			cout << endl;
+		}
+	}
+	return 0;
+}
