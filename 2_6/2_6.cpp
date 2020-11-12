77c34c0fe8261b93eb38bdb51626805682cd2bf6
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2020-11-13 02:18:33 +0500

    Update 2_6.cpp

--- 2_6/2_6.cpp
+++ 2_6/2_6.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int n; //длина основания.
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите длину основания:" << endl;
+	cin >> n;
+	int z = n;  //количество "*".
+
+	while (z >= 1)
+	{
+		for (int i = 1; i <= z; i++)
+		{
+			cout << "*";
+		}
+		z--;
+		cout << endl;
+	}
+	return 0;
+}
