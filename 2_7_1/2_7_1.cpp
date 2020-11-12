a7c72f55804e11a247dd23a1e152a8581e614e23
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2020-11-13 02:36:58 +0500

    Update 2_7_1.cpp

--- 2_7_1/2_7_1.cpp
+++ 2_7_1/2_7_1.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+int n, m;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cin >> n;
+	m = sqrt(n);
+	if ((n != m * m) or (n<=1))
+		cout << "Невозможно найти сторону";
+	else
+	{
+		for (int i = 1; i <= m; i++)
+		{
+			for (int i = 1; i <= m; i++)
+			{
+				cout << "* ";
+			}
+			cout << endl;
+		}
+	}
+	return 0;
+}
