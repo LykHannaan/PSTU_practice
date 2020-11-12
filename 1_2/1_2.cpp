1dff2ed9e82d01b97908b33a1482d38931d24454
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2020-11-13 01:56:50 +0500

    Update 1_2.cpp

--- 1_2/1_2.cpp
+++ 1_2/1_2.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
 
+float a, b;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    cin >> a >> b;
+
+    if (b != 0)
+    {
+        cout << a / b;
+    }
+    else
+    {
+        cout << "Не дели на ноль!";
+
+    }
+    return 0;
+}
