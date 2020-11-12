d4c9534e00931fecaeaf22f2a497c3333aa712e6
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2020-11-13 02:42:25 +0500

    Update 2_7_2.cpp

--- 2_7_2/2_7_2.cpp
+++ 2_7_2/2_7_2.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+int n,m;
+
+int main()
+{
+  setlocale(LC_ALL, "RUS");  
+    cin >> n;
+	
+	   for (int i=1; i<=n; i++)
+	    {
+		 for (int i=1; i<=n; i++)
+		  {
+		   cout << "* "; 
+		  }
+	     cout << endl;
+	    }	
+  return 0;
+}
