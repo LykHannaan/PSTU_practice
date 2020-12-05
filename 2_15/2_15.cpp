f2594044e99349a203e55fa2c87fb08d7360f5b9
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2020-12-06 00:16:41 +0500

    Update 2_15.cpp

--- 2_15/2_15.cpp
+++ 2_15/2_15.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int n,z,p,k=0;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cin >> n >> z;
+	 while (n > 0)
+	 {
+		p = n % 10;
+		n /= 10;
+		if (p == z) k = 1;;
+	 }
+	  if (k == 1) cout << "Yes";
+		else cout << "No";
+	 
+return 0;
+}
