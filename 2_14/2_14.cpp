fb99c4654df074f045cdc39d1b8b7a9d89e3fac4
LykHannaan <73856256+LykHannaan@users.noreply.github.com>
2020-12-06 00:09:26 +0500

    Update 2_14.cpp

--- 2_14/2_14.cpp
+++ 2_14/2_14.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int n;
+int n,sum;
 
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-	cin >> a >> b >> c;
-
-	cout << "Второй корень: " << x2;
-	
+	cin >> n;
+	 while (n > 0)
+	 {
+		sum += n % 10;
+		n /= 10;
+	 }
+	 cout << sum;
 return 0;
 }
